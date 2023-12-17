8de6b3332c54d65f9927f810c6a6cc5f5e958c51
Rahino Quijano <rahinoquijano@gmail.com>
2023-12-17 10:10:17 +0400

    feat: setup codebase, packages and files

---
+++ src/features/game/components/player-input-form.component.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export interface PlayerInputFormProps {}
+
+const PlayerInputForm: React.FC<PlayerInputFormProps> = (props) => {
+  const {} = props;
+
+  return <div></div>;
+};
+
+export default PlayerInputForm;
---
+++ src/features/game/components/ranking.component.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export interface RankingComponentProps {}
+
+const RankingComponent: React.FC<RankingComponentProps> = (props) => {
+  const {} = props;
+
+  return <div></div>;
+};
+
+export default RankingComponent;
